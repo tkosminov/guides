53648630233df7ba851ac71c48136d57198434bf
t.kosminov <t.kosminov@oblakogroup.ru>
2023-02-27 12:25:15 +0300

    add errors

--- postgres/04-physical-backup/wal-g/scripts/walg-backup-delete.sh
+++ postgres/04-physical-backup/wal-g/scripts/walg-backup-delete.sh
@@ -2,7 +2,23 @@
 
 source /var/lib/postgresql/base-config.sh
 
-su - postgres -c "/usr/local/bin/wal-g delete before FIND_FULL \$(date -d '-7 days' '+\\%FT\\%TZ') --config /var/lib/postgresql/.walg.json --confirm >> /var/log/postgresql/walg_delete.log 2>&1"
+trap 'catch $? $LINENO' ERR
+set -e
+catch() {
+  echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $1"
+
+  if [ ! -z "$TELEGRAM_CHAT_ID" ] && [ ! -z "$TELEGRAM_TOKEN" ]; then
+    curl -X POST \
+      -H 'Content-Type: application/json' \
+      -d "{\"chat_id\": \"${TELEGRAM_CHAT_ID}\", \"text\": \"🆘 При удалении физического бэкапа базы данных ${BACKUP_PROJECT_NAME} возникли ошибки\nError in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. ${BASH_COMMAND} exited with status $1\", \"disable_notification\": true}" \
+      https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage
+  else
+    echo "Physical Delete chat_id and token not provided"
+  fi
+}
+
+su - postgres -c "/usr/local/bin/wal-g delete before FIND_FULL \$(date -d '-7 days' +\"%Y-%m-%dT%H:%M:%SZ\") --config /var/lib/postgresql/.walg.json --confirm --use-sentinel-time >> /var/log/postgresql/walg_delete.log 2>&1"
+# su - postgres -c "/usr/local/bin/wal-g delete retain FULL 1 --config /var/lib/postgresql/.walg.json --confirm --use-sentinel-time >> /var/log/postgresql/walg_delete.log 2>&1"
 
 if [ ! -z "$TELEGRAM_CHAT_ID" ] && [ ! -z "$TELEGRAM_TOKEN" ]; then
   curl -X POST \
--- postgres/04-physical-backup/wal-g/scripts/walg-backup-push.sh
+++ postgres/04-physical-backup/wal-g/scripts/walg-backup-push.sh
@@ -4,6 +4,21 @@ echo "Physical Push Start"
 
 source /var/lib/postgresql/base-config.sh
 
+trap 'catch $? $LINENO' ERR
+set -e
+catch() {
+  echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $1"
+
+  if [ ! -z "$TELEGRAM_CHAT_ID" ] && [ ! -z "$TELEGRAM_TOKEN" ]; then
+    curl -X POST \
+      -H 'Content-Type: application/json' \
+      -d "{\"chat_id\": \"${TELEGRAM_CHAT_ID}\", \"text\": \"🆘 При создании физического бэкапа базы данных ${BACKUP_PROJECT_NAME} возникли ошибки\nError in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. ${BASH_COMMAND} exited with status $1\", \"disable_notification\": true}" \
+      https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage
+  else
+    echo "Physical Push chat_id and token not provided"
+  fi
+}
+
 # создаем и загружаем бэкап в облако
 su - postgres -c '/usr/local/bin/wal-g backup-push /var/lib/postgresql/13/main --config /var/lib/postgresql/.walg.json'
 
